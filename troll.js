3b8c74505177080a32a1d43bafbdce8225dc9212
coolgreatgamer <96445003+coolgreatgamer@users.noreply.github.com>
2024-05-12 10:44:11 -0700

    fixed keyboard function
    changed setting to if you press any key you go into fullscreen

--- troll.js
+++ troll.js
@@ -1,18 +1,41 @@
 let fs = fullscreen();
 
 function setup() {
+  alert("don't press s");
   new Canvas(500, 240);
   //alert("test");
 
 }
 
-function mousePressed() {
+//function mousePressed() {
    // if (kb.pressing('a')) {
-      let fs = fullscreen();
+      //let fs = fullscreen();
 
-      alert(fs);
+      //alert(fs);
 
-      fullscreen(!fs);
+      //fullscreen(!fs);
     
-//} 
+//}
+//}
+//function mousePressed() {
+//  if (kb.released('j')) {
+//    let fs = fullscreen();
+//    fullscreen(!fs);
+//  }
+//  if (kb.presses('j')) {
+//    let fs = fullscreen();
+//    fullscreen(!fs);
+//  } 
+//}
+
+function keyPressed() {
+  
+  if (keyIsPressed == true) {
+    let fs = fullscreen();
+    fullscreen(!fs);
+  }
+  if (keyIsPressed == false) {
+    let fs = fullscreen();
+    fullscreen(!fs);
+  }
 }
